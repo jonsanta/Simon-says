981ae23ffdd8965ee3943c432e69578d3ba5c387
jonsanta <jon.santamaria1995@gmail.com>
2023-01-03 14:42:20 +0100

    Refactor

--- game.js
+++ game.js
@@ -1,17 +1,18 @@
 const colors = ["green", "red", "yellow", "blue"]
 
 var order = new Array()
-var index
+var turnIndex
 
 class Round {	
 	constructor(){
-		colors.forEach(color => document.getElementById(color).addEventListener("click", this.selectColor))
+		turnIndex = 0
 		order.push(String(colors[Math.floor(Math.random() * 4)]))
 		document.getElementById("inside-circle").innerHTML = String(order.length)
 		this.play()
 	}
 	
 	play(){
+		colors.forEach(color => document.getElementById(color).addEventListener("click", this.selectColor))
 		colors.forEach(color => document.getElementById(color).style.pointerEvents = "none")
 		order.forEach((color, index) => { setTimeout (() => this.light(color, index), 1000 * index) })
 	}
@@ -25,15 +26,15 @@ class Round {
 	}
 
 	selectColor(evt){
-		if(index <= order.length){
+		if(turnIndex <= order.length){
 			let color = evt.currentTarget.id;
 			lightAnimation(color, "all 0.3s", "brightness(110%)")
 			setTimeout(() => lightAnimation(color, "all 0.3s", "brightness(70%)"), 200)
 		}
 		
-		if(evt.currentTarget.id != order[index]) gameOver()
-		else if(index + 1 == order.length) newRound()
-		else index++
+		if(evt.currentTarget.id != order[turnIndex]) gameOver()
+		else if(turnIndex + 1 == order.length) newRound()
+		else turnIndex++
 	}
 }
 
@@ -43,13 +44,10 @@ function lightAnimation(color, transition, filter){
 }
 
 function newRound(){
-	index = 0
 	document.getElementById('start-btn').style.display = "none"
-	setTimeout(() => window.game = new Round(), 800)
+	setTimeout(() => new Round(), 800)
 }
 
 function gameOver(){
 	swal('Game Over','', 'error').then (()=> { location.reload() })
 }
-
-
